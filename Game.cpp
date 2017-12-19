27560749b9409e06cd626dbce4735b04fd319f87
maddysrg4 <maddysrg4@gmail.com>
2017-12-19 12:38:26 +0530

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 #include<limits>
 using namespace std;
-
+class Pawn;
 class GameBoard
 {
     public:
@@ -52,7 +52,7 @@ class GameBoard
 class Heros
 {
     public:
-        virtual  GameBoard* move(string s,int player,GameBoard* g)=0;
+        virtual  GameBoard* move(string s,int player,GameBoard* g,Pawn *p1,Pawn *p2)=0;
 };
 class Coordinates
 {
@@ -76,7 +76,7 @@ class Pawn: public Heros
             }
            // g->Board[i][j]=name;
         }
-        GameBoard* move(string s,int player,GameBoard *g)
+        GameBoard* move(string s,int player,GameBoard *g,Pawn *p1,Pawn *p2)
         {
             int i=p.x;
             int j=p.y;
@@ -94,9 +94,18 @@ class Pawn: public Heros
                 else if(s[3]=='L'){j++;}
                 else if(s[3]=='R'){j--;}
             }
-         /*   if(g->Board[i][j][0]!='-'){
+            if(g->Board[i][j][0]!='-'){
+                    int p=s[1]-'0';
+                    if(player==2){
+                    p1[p-1].p.x=-1;
+                    p1[p-1].p.y=-1;}
+                    else
+                    {
+                        p2[p-1].p.x=-1;
+                        p2[p-1].p.y=-1;
+                    }
 
-            }*/
+            }
             g->Board[i][j]=g->Board[p.x][p.y];
             g->Board[p.x][p.y]="-";
             p.x=i;
@@ -107,12 +116,13 @@ class Pawn: public Heros
         {
             int i=p.x;
             int j=p.y;
+
             if(player==1)
             {
-                if(s[3]=='F'){i-1<0;return 1;}
-                else if(s[3]=='B'){i+1>4;return 1;}
-                else if(s[3]=='L'){j-1<0;return 1;}
-                else if(s[3]=='R'){j+1>4;return 1;}
+                if(s[3]=='F'){if(i-1<0){return 1;}}
+                else if(s[3]=='B'){if(i+1>4){return 1;}}
+                else if(s[3]=='L'){if(j-1<0){return 1;}}
+                else if(s[3]=='R'){if(j+1>4){return 1;}}
                 if(s[3]=='F'){if(g->Board[i-1][j][0]=='A') return 2;}
                 else if(s[3]=='B'){if(g->Board[i+1][j][0]=='A') return 2;}
                 else if(s[3]=='L'){if(g->Board[i][j-1][0]=='A') return 2;}
@@ -120,14 +130,14 @@ class Pawn: public Heros
             }
             else
             {
-                if(s[3]=='F'){i+1>4;return 1;}
-                else if(s[3]=='B'){i-1<0;return 1;}
-                else if(s[3]=='L'){j+1>4;return 1;}
-                else if(s[3]=='R'){j-1<0;return 1;}
-                if(s[3]=='F'){if(g->Board[i+1][j][0]=='A') return 2;}
-                else if(s[3]=='B'){if(g->Board[i-1][j][0]=='A') return 2;}
-                else if(s[3]=='L'){if(g->Board[i][j+1][0]=='A') return 2;}
-                else if(s[3]=='R'){if(g->Board[i][j-1][0]=='A') return 2;}
+                if(s[3]=='F'){if(i+1>4){return 1;}}
+                else if(s[3]=='B'){if(i-1<0){return 1;}}
+                else if(s[3]=='L'){if(j+1>4){return 1;}}
+                else if(s[3]=='R'){if(j-1<0){return 1;}}
+                if(s[3]=='F'){if(g->Board[i+1][j][0]=='B') return 2;}
+                else if(s[3]=='B'){if(g->Board[i-1][j][0]=='B') return 2;}
+                else if(s[3]=='L'){if(g->Board[i][j+1][0]=='B') return 2;}
+                else if(s[3]=='R'){if(g->Board[i][j-1][0]=='B') return 2;}
             }
             return 0;
         }
@@ -140,7 +150,8 @@ int main()
    // string As[5]={"P3","P2","P5","P4","P1"},Bs[5]={"P2","P1","P3","P5","P4"};
 
     GameBoard* game= new GameBoard();
-    Pawn p1[50],p2[50];
+//    Pawn* p1=new Pawn[50];
+    Pawn p1[5],p2[5];
     for(i=0;i<5;i++)
     {
         string s;
@@ -174,11 +185,12 @@ int main()
             string s;
             cin>>s;
             int p=s[1]-'0';
-
+            if(p1[p-1].p.x==-1){cout<<"Player Already Dead\n";continue;}
             int error=p1[p-1].ErrorCheck(s,1,game);
-            if(error==1){cout<<"Out of Board";continue;}
-            else if(error==2){cout<<"Own Player standing";continue;}
-            game=p1[p-1].move(s,1,game);
+            if(error==1){cout<<"Out of Board\n";continue;}
+            else if(error==2){cout<<"Own Player standing\n";continue;}
+
+            game=p1[p-1].move(s,1,game,p1,p2);
             game->DisplayBoard();
             turn=2;
         }
@@ -188,10 +200,11 @@ int main()
             string s;
             cin>>s;
             int p=s[1]-'0';
+            if(p1[p-1].p.x==-1){cout<<"Player Already Dead\n";continue;}
             int error=p2[p-1].ErrorCheck(s,2,game);
-            if(error==1){cout<<"Out of Board";continue;}
-            else if(error==2){cout<<"Own Player standing";continue;}
-            game=p2[p-1].move(s,2,game);
+            if(error==1){cout<<"Out of Board\n";continue;}
+            else if(error==2){cout<<"Own Player standing\n";continue;}
+            game=p2[p-1].move(s,2,game,p1,p2);
             game->DisplayBoard();
             turn=1;
         }
