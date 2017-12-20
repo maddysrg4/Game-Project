5b3f11dcfc3e0db1a67c0a219703ba73daee66b6
Madhusudan Saraogi <maddysrg4@gmail.com>
2017-12-20 19:34:50 +0530

    Update main.java

--- main.java
+++ main.java
@@ -2,11 +2,16 @@ import java.util.*;
 
 public class main{  
     public static void main(String args[]){  
-     	Scanner input=new Scanner(System.in);
+     	
      	GameBoard game=new GameBoard();
      	Heros[] p1=new Heros[5];
      	Heros[] p2=new Heros[5];
-     	System.out.println("Player A input");
+     	takeinput(game,p1,p2);
+    }  
+    public static void takeinput(GameBoard game,Heros p1[],Heros p2[])
+{
+		Scanner input=new Scanner(System.in);
+	System.out.println("Player A input");
      	int i,j;
      	for(i=0;i<5;i++)
      	{
@@ -29,11 +34,7 @@ public class main{
      	}
      	int turn=1;
      	game.DisplayBoard();
-     	for(i=0;i<5;i++)
-     	{
-     		System.out.println(p2[i].PlayerStatus);
-
-     	}
+     	
      	while(true)
      	{
      		if(turn==1)
@@ -44,7 +45,6 @@ public class main{
      			String tem="A-"+s.substring(0,2);
      			for(i=0;i<5;i++)
      			{
-     				     //	System.out.println(p1[i].name+"   "+tem);
 
      				if(p1[i].name.equals(tem)&&p1[i].PlayerStatus)
 					{
@@ -87,5 +87,5 @@ public class main{
         if(p==1){System.out.println("Player A wins\n");break;}
         else if(p==2){System.out.println("Player B wins\n");break;}
      	}
-    }  
+}
 }  
